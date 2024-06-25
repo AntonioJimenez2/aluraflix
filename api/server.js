b2870b0c57904bd8558717d3372dbd331b7c96a7
AntonioJimenez2 <antjimz@gmail.com>
2024-06-25 11:08:03 -0600

    nueva prueba de funcionamiento de api

--- api/server.js
+++ api/server.js
@@ -1,9 +1,18 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('./db.json'); // Asegúrate de que la ruta es correcta
-const middlewares = jsonServer.defaults();
 
-server.use(middlewares);
-server.use(router);
 
-module.exports = server;
+// See https://github.com/typicode/json-server#module
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('./db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+// Add this before server.use(router)
+
+server.use(router)
+server.listen(3000, () => {
+    console.log('JSON Server is running')
+})
+
+// Export the Server API
+module.exports = server
