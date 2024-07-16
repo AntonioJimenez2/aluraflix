3523ac27d1871939f09c254ae3190859deca0e7a
AntonioJimenez2 <antjimz@gmail.com>
2024-07-16 12:24:11 -0600

    mejorada version movil y añadido icono de linkedin

--- src/components/Pie/index.jsx
+++ src/components/Pie/index.jsx
@@ -1,12 +1,14 @@
 import styles from "./Pie.module.css";
 import logo from "../../assets/img/logo_black.png";
+import { FaLinkedin } from "react-icons/fa";
 
 const Pie = () => {
   return (
     <>
       <footer className={styles.pie}>
         <img src={logo} alt="logo alura" />
-        <p>Creado por Antonio Jiménez - Alura-ONE 2024</p>
+        <p>Creado por Antonio Jiménez  <a href="https://www.linkedin.com/in/juan-antonio-jimenez-orozco/" target="_blank" ><FaLinkedin /></a>  - Alura-ONE 2024</p>
+        
       </footer>
     </>
   );
